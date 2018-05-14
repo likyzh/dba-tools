231cbd80c39b715958d271fb77f3163fad96c810
likyzh <tianyuanzhao@126.com>
2018-05-14 09:57:01 +0800

    Update vars_conf.sh

--- common/vars_conf.sh
+++ common/vars_conf.sh
@@ -5,10 +5,10 @@
 # global variables
 # ################################################################
 # 可修改的变量
-MYSQL_CONFIG_DIR="/home/dba/mysqlnode"
-MYSQL_DIR="/home/dba/mysql"
-MONGO_DIR="/home/dba/mongodb"
-PYTHON_DIR="/home/dba/python"
+MYSQL_CONFIG_DIR="/home/ddb/mysqlnode"
+MYSQL_DIR="/home/ddb/mysql"
+MONGO_DIR="/home/ddb/mongodb"
+PYTHON_DIR="/home/ddb/python"
 MYSQL_USER="root"
 MONGO_USER="admin"
 DDB_USER="sys"
